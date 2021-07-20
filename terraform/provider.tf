42128530b2ad2a049d2c880b5ff8918f3605a0e4
root <root@ip-172-31-6-109.us-east-2.compute.internal>
2021-07-20 19:15:51 +0000

    file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,2 @@
 provider "aws" {
-   region = var.region 
-access_key = "AKIASMUF6N6NDTN34NGH"
-secret_key = "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"
 }
--- terraform/var.tf
+++ terraform/var.tf
@@ -1,11 +1,4 @@
 
-variable "region" {
-  description = "The AWS region to use"
-}
-
-variable "profile" {
-  description = "The AWS profile to use"
-}
 variable "instance_count" {
   description = "Number of instance"
 }
