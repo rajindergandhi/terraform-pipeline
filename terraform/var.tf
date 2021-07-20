8d0968f26860d35dada5f541f2efb423b4912ab1
root <root@ip-172-31-6-109.us-east-2.compute.internal>
2021-07-20 17:31:29 +0000

    modified var file

--- terraform/var.tf
+++ terraform/var.tf
@@ -1,11 +1,9 @@
 variable "web_instance_type" {
   description = "Choose instance type for your web"
-  type        = "string"
   default     = "t2.micro"
 }
 
 variable "web_tags" {
-  type = "map"
   default = {
     Name = "Webserver"
   }
@@ -13,13 +11,11 @@ variable "web_tags" {
 
 variable "web_ec2_count" {
   description = "Choose number ec2 instances for web"
-  type        = "string"
   default     = "1"
 }
 
 variable "region" {
   description = "Choose region for your stack"
-  type        = "string"
   default     = "us-east-1"
 }
 
